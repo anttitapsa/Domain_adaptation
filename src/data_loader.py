f03fbc041c1152f73623fb96a455f3c03c5413b4
Tuomas Poutanen <tuomas.poutanen@gmail.com>
2022-02-16 14:42:32 +0200

    Create dataset for target data

--- src/data_loader.py
+++ src/data_loader.py
@@ -1,9 +1,31 @@
 # File for adding data handelers
 
 from torch.utils.data import Dataset, DataLoader
+from torchvision.io import read_image
+import os
 
-class CellDataset(Dataset):
+
+class TargetDataset(Dataset):
+    """ This dataset contains only target domain images that do not have labels """
+    def __init__(self):
+        # data loading
+        self.img_dir = os.path.join(os.getcwd(), "data", "target")
+
     
+    def __getitem__(self, idx):
+        # return item with possible transforms
+        img_path = os.path.join(self.img_dir, os.listdir(self.img_dir)[idx])
+        image = read_image(img_path)
+        label = None
+        return image, label
+        
+    def __len__(self):
+        # return len(dataset)
+        return len(os.listdir(self.img_dir))
+
+
+class SourceDataset(Dataset):
+    """ This dataset contains source domain images and labels """
     def __init__(self):
         # data loading
         pass
