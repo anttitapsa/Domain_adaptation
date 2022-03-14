3ef6b67d4c47c264704370097f3e3519248d28e3
Antti Huttunen <antti.t.huttunen@aalto.fi>
2022-03-14 17:32:20 +0200

    fixed typos

--- src/CycleGan.py
+++ src/CycleGan.py
@@ -209,10 +209,10 @@ if __name__ == '__main__':
     epochs = 10
     criterion = nn.L1Loss()
 
-    optimizer_D_A = torch.optim.Adam(D_A.Parameters(),lr=0.0002, betas=0.5)
-    optimizer_D_B = torch.optim.Adam(D_B.Parameters(),lr=0.0002, betas=0.5)
-    optimizer_G_A2B = torch.optim.Adam(G_A2B.Parameters(),lr=0.0002, betas=0.5)
-    optimizer_G_B2A = torch.optim.Adam(G_B2A.Parameters(),lr=0.0002, betas=0.5)
+    optimizer_D_A = torch.optim.Adam(D_A.parameters(),lr=0.0002, betas=0.5)
+    optimizer_D_B = torch.optim.Adam(D_B.parameters(),lr=0.0002, betas=0.5)
+    optimizer_G_A2B = torch.optim.Adam(G_A2B.parameters(),lr=0.0002, betas=0.5)
+    optimizer_G_B2A = torch.optim.Adam(G_B2A.parameters(),lr=0.0002, betas=0.5)
 
     print("Starting Training Loop...")
 
