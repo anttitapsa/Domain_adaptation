f3559b05a45754a677d2a0ef6e826a4ab39c29d6
Amanda Aarnio <aarnioa5@aalto.fi>
2022-03-15 13:14:25 +0200

    noise file added

---
+++ src/noise.py
@@ -0,0 +1,32 @@
+import numpy as np
+import cv2
+from skimage.util import random_noise
+from PIL import Image
+import torch
+import torchvision.transforms as Trolli
+
+def add_noise_to_images(noise_type, image):
+    if noise_type == 'Gaussian':
+        gauss = np.random.normal(0,1,image.size)
+        gauss = gauss.reshape(image.shape[0],image.shape[1]).astype('uint8') 
+        noise_image = cv2.add(image, gauss)
+        
+    elif noise_type == 'Speckle':
+        gauss = np.random.normal(0,1,image.size)
+        gauss = gauss.reshape(image.shape[0],image.shape[1]).astype('uint8')
+        noise_image = image + image * gauss
+    
+    elif noise_type == 'Random':
+        noise_image = random_noise(image, mode='s&p',amount=0.3)
+    
+    elif noise_type == 'Poisson':
+        vals = len(np.unique(image))
+        vals = 2 ** np.ceil(np.log2(vals))
+        noise_image = np.random.poisson(image * vals) / float(vals)
+        
+    
+    # random flip the image
+    ''''vflipper = Trolli.RandomVerticalFlip(p=0.5)
+    transformed_image = [vflipper(noise_image) for _ in range(4)]'''
+    return noise_image
+
