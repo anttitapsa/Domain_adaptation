ea766ca5d12a8c8877c796f8e281f85cad3ab323
Lauri Pohjola <lauri.pohjola@helsinki.fi>
2022-03-17 12:50:23 +0200

    GRL in unet

--- src/model.py
+++ src/model.py
@@ -19,6 +19,9 @@ class Unet(Module):
     # dropout to prevent overtraining
     def __init__(self, numChannels = 1, classes = 2, dropout = 0.1):
         super(Unet, self).__init__()
+        
+        self.domain_classifier = domain_classifier(in_channel=1024)
+        
         # Encoder (traditional convolutional and max pooling layers)
         self.conv1 = Unet._conv2d_block(in_channel = numChannels, out_channel = 64)
         self.maxpool1 = MaxPool2d(kernel_size = 2, stride = 2)
@@ -58,6 +61,7 @@ class Unet(Module):
         
         self.outc = Conv2d(in_channels = 64, out_channels = classes, kernel_size = 1)
         
+        
     def forward(self, x):
         # function that defines how the model is going to be run, from input to output
         x1 = self.conv1(x)
@@ -76,9 +80,11 @@ class Unet(Module):
         x = self.maxpool4(x4)
         x = self.dropout4(x)
         
-        x = self.conv5(x)
+        x5 = self.conv5(x)
+        #x = 
+        y = self.domain_classifier(x5)
         
-        x6 = self.transpose6(x)
+        x6 = self.transpose6(x5)
         x = cat((x6, x4), dim = 1)
         x = self.dropout6(x)
         x = self.conv6(x)
@@ -99,7 +105,7 @@ class Unet(Module):
         x = self.conv9(x)
         
         x = self.outc(x)
-        return torch.sigmoid(x)
+        return torch.sigmoid(x), y
 
     @staticmethod
     def _conv2d_block(in_channel, out_channel):
@@ -112,95 +118,28 @@ class Unet(Module):
             ReLU(inplace = True)
         )
    
-   
-# Ready model for unet with GRL from https://github.com/eliottbrion/unsupervised-domain-adaptation-unet-keras/blob/master/models.py
-def unet_L6(params):
+class domain_classifier(nn.Module):
+    def __init__(self, in_channel):
+        super(domain_classifier, self).__init__()
+        self.fc1 = nn.Linear(in_channel, 100) 
+        self.fc2 = nn.Linear(100, 1)
+        self.drop = nn.Dropout2d(0.25)
 
-    volumes = Input(batch_shape=(None, *image_size, 1))
+    def forward(self, x):
+        x = grad_reverse(x)
+        x = F.leaky_relu(self.drop(self.fc1(x)))
+        x = self.fc2(x)
+        return F.sigmoid(x)
+      
+class GradReverse(Function):
+    @staticmethod
+    def forward(ctx, x):
+        return x.view_as(x)
 
-    # Downsampling
+    @staticmethod
+    def backward(ctx, grad_output):
+        return grad_output.neg()
 
-    conv1 = Conv3D(params['n_feat_maps'], (3, 3, 3), activation='relu', padding='same')(volumes)
-    conv1 = Conv3D(params['n_feat_maps'], (3, 3, 3), activation='relu', padding='same')(conv1)
-    pool1 = MaxPooling3D(pool_size=(2, 2, 2))(conv1)
+def grad_reverse(x):
+    return GradReverse.apply(x)
 
-    conv2 = Conv3D(params['n_feat_maps'] * 2, (3, 3, 3), activation='relu', padding='same')(pool1)
-    conv2 = Conv3D(params['n_feat_maps'] * 2, (3, 3, 3), activation='relu', padding='same')(conv2)
-    pool2 = MaxPooling3D(pool_size=(2, 2, 2))(conv2)
-
-    conv3 = Conv3D(params['n_feat_maps'] * 4, (3, 3, 3), activation='relu', padding='same')(pool2)
-    conv3 = Conv3D(params['n_feat_maps'] * 4, (3, 3, 3), activation='relu', padding='same')(conv3)
-    pool3 = MaxPooling3D(pool_size=(2, 2, 2))(conv3)
-
-    conv4 = Conv3D(params['n_feat_maps'] * 8, (3, 3, 3), activation='relu', padding='same')(pool3)
-    conv4 = Conv3D(params['n_feat_maps'] * 8, (3, 3, 3), activation='relu', padding='same')(conv4)
-    pool4 = MaxPooling3D(pool_size=(2, 2, 2))(conv4)
-
-    conv5 = Conv3D(params['n_feat_maps'] * 16, (3, 3, 3), activation='relu', padding='same')(pool4)
-    conv5 = Conv3D(params['n_feat_maps'] * 16, (3, 3, 3), activation='relu', padding='same')(conv5)
-    pool5 = MaxPooling3D(pool_size=(2, 2, 2))(conv5)
-
-    # Bottleneck
-
-    conv6 = Conv3D(params['n_feat_maps'] * 32, (3, 3, 3), activation='relu', padding='same')(pool5)
-    conv6 = Conv3D(params['n_feat_maps'] * 32, (3, 3, 3), activation='relu', padding='same')(conv6)
-    model_feature = conv6
-    conv6 = tf.slice(conv6, [0, 0, 0, 0, 0], [params['batch_size'] // 2, 5, 5, 4, params['n_feat_maps'] * 32])
-
-    # Upsampling
-
-    copy5 = tf.slice(conv5, [0, 0, 0, 0, 0], [params['batch_size'] // 2, 10, 10, 8, params['n_feat_maps'] * 16])
-    up7 = concatenate(
-        [Conv3DTranspose(params['n_feat_maps'] * 16, (2, 2, 2), strides=(2, 2, 2), padding='same')(conv6), copy5], axis=4)
-    conv7 = Conv3D(params['n_feat_maps'] * 16, (3, 3, 3), activation='relu', padding='same')(up7)
-    conv7 = Conv3D(params['n_feat_maps'] * 16, (3, 3, 3), activation='relu', padding='same')(conv7)
-
-    copy4 = tf.slice(conv4, [0, 0, 0, 0, 0], [params['batch_size'] // 2, 20, 20, 16, params['n_feat_maps'] * 8])
-    up8 = concatenate(
-      [Conv3DTranspose(params['n_feat_maps'] * 8, (2, 2, 2), strides=(2, 2, 2), padding='same')(conv7), copy4], axis=4)
-    conv8 = Conv3D(params['n_feat_maps'] * 8, (3, 3, 3), activation='relu', padding='same')(up8)
-    conv8 = Conv3D(params['n_feat_maps'] * 8, (3, 3, 3), activation='relu', padding='same')(conv8)
-
-    copy3 = tf.slice(conv3, [0, 0, 0, 0, 0], [params['batch_size'] // 2, 40, 40, 32, params['n_feat_maps'] * 4])
-    up9 = concatenate(
-      [Conv3DTranspose(params['n_feat_maps'] * 4, (2, 2, 2), strides=(2, 2, 2), padding='same')(conv8), copy3], axis=4)
-    conv9 = Conv3D(params['n_feat_maps'] * 4, (3, 3, 3), activation='relu', padding='same')(up9)
-    conv9 = Conv3D(params['n_feat_maps'] * 4, (3, 3, 3), activation='relu', padding='same')(conv9)
-
-    copy2 = tf.slice(conv2, [0, 0, 0, 0, 0], [params['batch_size'] // 2, 80, 80, 64, params['n_feat_maps'] * 2])
-    up10 = concatenate(
-      [Conv3DTranspose(params['n_feat_maps'] * 2, (2, 2, 2), strides=(2, 2, 2), padding='same')(conv9), copy2], axis=4)
-    conv10 = Conv3D(params['n_feat_maps'] * 2, (3, 3, 3), activation='relu', padding='same')(up10)
-    conv10 = Conv3D(params['n_feat_maps'] * 2, (3, 3, 3), activation='relu', padding='same')(conv10)
-
-    copy1 = tf.slice(conv1, [0, 0, 0, 0, 0], [params['batch_size'] // 2, 160, 160, 128, params['n_feat_maps'] * 1])
-    up11 = concatenate(
-      [Conv3DTranspose(params['n_feat_maps'] * 1, (2, 2, 2), strides=(2, 2, 2), padding='same')(conv10), copy1], axis=4)
-    conv11 = Conv3D(params['n_feat_maps'] * 1, (3, 3, 3), activation='relu', padding='same')(up11)
-    conv11 = Conv3D(params['n_feat_maps'] * 1, (3, 3, 3), activation='relu', padding='same')(conv11)
-
-    segmentation_pred = Conv3D(2, (1, 1, 1), activation = 'softmax', name="segmentation_output")(conv11)
-
-    # === Domain classifier ===
-
-    feat = GradReverse(0.)(model_feature)
-
-    convA = Conv3D(params['d_arch'][0], (3, 3, 3), activation='relu', padding='same')(feat)
-    convA = Conv3D(params['d_arch'][0], (3, 3, 3), activation='relu', padding='same')(convA)
-    fl = Flatten()(convA)
-    dense = Dense(params['d_arch'][1], activation='relu')(fl)
-
-    domain_pred = Dense(1, activation='sigmoid', name='domain_output')(dense)
-
-    model = Model(inputs=[volumes], outputs=[segmentation_pred, domain_pred])
-
-    losses = {
-        "segmentation_output": dice_loss,
-        "domain_output": "binary_crossentropy",
-    }
-    lossWeights = {"segmentation_output": 1.0, "domain_output": params['psi']}
-
-    model.compile(optimizer=Adam(params['lr']), loss=losses, loss_weights=lossWeights,
-                  metrics={'segmentation_output': None, 'domain_output': 'accuracy'})
-
-    return model
