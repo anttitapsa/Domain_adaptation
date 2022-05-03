2b14f170dc1d45e70201e2dc11cd19657b15c835
Huttunen Antti <huttuna6@haifu.org.aalto.fi>
2022-05-03 23:17:50 +0300

    fixed parameters in new cyclegan

--- src/new_cyclegan.py
+++ src/new_cyclegan.py
@@ -9,37 +9,37 @@ using Cycle-Consistent Adversarial Networks": https://arxiv.org/pdf/1703.10593.p
 and the model is originally implemented in https://github.com/junyanz/pytorch-CycleGAN-and-pix2pix
 """
 class UnetGenerator(nn.Module):
-    def __init__(self, input_nc, output_nc, num_downs, ngf=64, norm_layer = nn.BatchNorm2d, use_dropout=False):
+    def __init__(self, input_nc, output_nc, num_downs=3, ngf=64, norm_layer = nn.BatchNorm2d, use_dropout=False):
         super(UnetGenerator, self).__init__()
-        unet_block = UnetSkipConnectionBLock(output_nc= ngf*8,
+        unet_block = UnetSkipConnectionBLock(outer_nc= ngf*8,
                                              inner_nc=ngf*8,
                                              input_nc= None,
                                              submodule= None,
                                              norm_layer=norm_layer,
                                              innermost = True)   
         for i in range(num_downs - 5):
-            unet_block = UnetSkipConnectionBLock(output_nc= ngf*8,
+            unet_block = UnetSkipConnectionBLock(outer_nc= ngf*8,
                                                 inner_nc=ngf*8,
                                                 input_nc= None,
                                                 submodule= unet_block,
                                                 norm_layer=norm_layer,
                                                 use_dropout=use_dropout)
-        unet_block = UnetSkipConnectionBLock(output_nc= ngf*4,
+        unet_block = UnetSkipConnectionBLock(outer_nc= ngf*4,
                                              inner_nc=ngf*8,
                                              input_nc= None,
                                              submodule= unet_block,
                                              norm_layer=norm_layer)
-        unet_block = UnetSkipConnectionBLock(output_nc= ngf*2,
+        unet_block = UnetSkipConnectionBLock(outer_nc= ngf*2,
                                              inner_nc=ngf*4,
                                              input_nc= None,
                                              submodule= unet_block,
                                              norm_layer=norm_layer)
-        unet_block = UnetSkipConnectionBLock(output_nc= ngf,
+        unet_block = UnetSkipConnectionBLock(outer_nc= ngf,
                                              inner_nc=ngf*2,
                                              input_nc= None,
                                              submodule= unet_block,
                                              norm_layer=norm_layer)
-        self.model = UnetSkipConnectionBLock(output_nc= output_nc,
+        self.model = UnetSkipConnectionBLock(outer_nc= output_nc,
                                              inner_nc=ngf,
                                              input_nc= input_nc,
                                              submodule= unet_block,
@@ -50,11 +50,12 @@ class UnetGenerator(nn.Module):
         return self.model(input)
 
 class UnetSkipConnectionBLock(nn.Module):
-    def __inti__(self, outer_nc, inner_nc, input_nc= None, submodule=None, outermost= False, innermost = False, norm_layer=nn.BatchNorm2d, use_dropout= False):
+    def __init__(self, outer_nc, inner_nc, input_nc= None, submodule=None, outermost= False, innermost = False, norm_layer=nn.BatchNorm2d, use_dropout= False):
         super(UnetSkipConnectionBLock, self).__init__()
         self.outermost = outermost
         use_bias = norm_layer == nn.BatchNorm2d
-
+        if input_nc is None:
+            input_nc=outer_nc
         downconv = nn.Conv2d(in_channels=input_nc,
                             out_channels=inner_nc,
                             kernel_size=4,
@@ -138,7 +139,7 @@ class NLayerDiscriminator(nn.Module):
         nf_mult_prev = nf_mult
         nf_mult = min(2 ** n_layers, 8)
         sequence += [nn.Conv2d(in_channels= ndf * nf_mult_prev,
-                                ou_channels= ndf * nf_mult, 
+                                out_channels= ndf * nf_mult, 
                                 kernel_size=kw, 
                                 stride=1, 
                                 padding=padw, 
@@ -156,7 +157,7 @@ class NLayerDiscriminator(nn.Module):
     def forward(self, input):
         return self.model(input)
 
-class CycleGan(UnetGenerator, NLayerDiscriminator):
+class CycleGan(nn.Module):
     def __init__(self, input_nc, output_nc,lr, beta1, device, ngf=64, ndf=64):
         super(CycleGan, self).__init__()
         self.device = device
@@ -182,8 +183,8 @@ class CycleGan(UnetGenerator, NLayerDiscriminator):
 
         self.optimizer_G = torch.optim.Adam(list(self.netG_A.parameters()) + list(self.netG_B.parameters()), lr=lr, betas=(beta1, 0.999))
         self.optimizer_D = torch.optim.Adam(list(self.netD_A.parameters()) + list(self.netD_B.parameters()), lr=lr, betas=(beta1, 0.999))
-        self.optimizers.append(self.optimizer_G)
-        self.optimizers.append(self.optimizer_D)
+        
+        self.optimizers = [self.optimizer_D, self.optimizer_G]
 
     def forward(self, real_A, real_B):
         self.fake_B = self.netG_A(real_A.to(self.device))  # G_A(A)
@@ -195,37 +196,37 @@ class CycleGan(UnetGenerator, NLayerDiscriminator):
 
         # Real
         pred_real = netD(real)
-        loss_D_real = self.criterionGAN(pred_real, True)
+        loss_D_real = self.criterionGAN(pred_real, real)
         # Fake
         pred_fake = netD(fake.detach())
-        loss_D_fake = self.criterionGAN(pred_fake, False)
+        loss_D_fake = self.criterionGAN(pred_fake, fake)
         # Combined loss and calculate gradients
         loss_D = (loss_D_real + loss_D_fake) * 0.5
         loss_D.backward()
         return loss_D
 
-    def backward_D_A(self):
-        self.loss_D_A = self.backward_D_basic(self.netD_A, self.real_B, self.fake_B)
+    def backward_D_A(self, real_B):
+        self.loss_D_A = self.backward_D_basic(self.netD_A, real_B, self.fake_B)
 
-    def backward_D_B(self):
-        self.loss_D_B = self.backward_D_basic(self.netD_B, self.real_A, self.fake_A)
+    def backward_D_B(self, real_A):
+        self.loss_D_B = self.backward_D_basic(self.netD_B, real_A, self.fake_A)
 
-    def backward_G(self):
+    def backward_G(self, real_A, real_B):
         # G_A should be identity if real_B is fed: ||G_A(B) - B||
-        self.idt_A = self.netG_A(self.real_B)
-        self.loss_idt_A = self.criterionIdt(self.idt_A, self.real_B) * 10 * 0.5
+        self.idt_A = self.netG_A(real_B)
+        self.loss_idt_A = self.criterionIdt(self.idt_A, real_B) * 10 * 0.5
         # G_B should be identity if real_A is fed: ||G_B(A) - A||
-        self.idt_B = self.netG_B(self.real_A)
-        self.loss_idt_B = self.criterionIdt(self.idt_B, self.real_A) * 10 * 0.5
+        self.idt_B = self.netG_B(real_A)
+        self.loss_idt_B = self.criterionIdt(self.idt_B, real_A) * 10 * 0.5
 
          # GAN loss D_A(G_A(A))
-        self.loss_G_A = self.criterionGAN(self.netD_A(self.fake_B), True)
+        self.loss_G_A = self.criterionGAN(self.netD_A(self.fake_B), real_B)
         # GAN loss D_B(G_B(B))
-        self.loss_G_B = self.criterionGAN(self.netD_B(self.fake_A), True)
+        self.loss_G_B = self.criterionGAN(self.netD_B(self.fake_A), real_A)
         # Forward cycle loss || G_B(G_A(A)) - A||
-        self.loss_cycle_A = self.criterionCycle(self.rec_A, self.real_A) * 10
+        self.loss_cycle_A = self.criterionCycle(self.rec_A, real_A) * 10
         # Backward cycle loss || G_A(G_B(B)) - B||
-        self.loss_cycle_B = self.criterionCycle(self.rec_B, self.real_B) * 10
+        self.loss_cycle_B = self.criterionCycle(self.rec_B, real_B) * 10
         # combined loss and calculate gradients
         self.loss_G = self.loss_G_A + self.loss_G_B + self.loss_cycle_A + self.loss_cycle_B + self.loss_idt_A + self.loss_idt_B
         self.loss_G.backward()
@@ -234,16 +235,18 @@ class CycleGan(UnetGenerator, NLayerDiscriminator):
         # forward
         self.forward(real_A, real_B)      # compute fake images and reconstruction images.
         # G_A and G_B
-        self.set_requires_grad([self.netD_A, self.netD_B], False)  # Ds require no gradients when optimizing Gs
+        self.netD_A.requires_grad_(False) 
+        self.netD_B.requires_grad_(False)  # Ds require no gradients when optimizing Gs
         self.optimizer_G.zero_grad()  # set G_A and G_B's gradients to zero
-        self.backward_G()             # calculate gradients for G_A and G_B
+        self.backward_G(real_A, real_B)             # calculate gradients for G_A and G_B
         self.optimizer_G.step()       # update G_A and G_B's weights
         # D_A and D_B
-        self.set_requires_grad([self.netD_A, self.netD_B], True)
+        self.netD_A.requires_grad_(True) 
+        self.netD_B.requires_grad_(True)
         self.optimizer_D.zero_grad()   # set D_A and D_B's gradients to zero
-        self.backward_D_A()      # calculate gradients for D_A
-        self.backward_D_B()      # calculate graidents for D_B
-        self.optimizer_D.step()  # update D_A and D_B's weights
+        self.backward_D_A(real_B)      # calculate gradients for D_A
+        self.backward_D_B(real_A)      # calculate graidents for D_B
+        self.optimizer_D.step() # update D_A and D_B's weights
 
 ########################################
 
@@ -305,15 +308,17 @@ if __name__ == '__main__':
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
     model = CycleGan(input_nc=1,
-                    output_nc=1,
-                    lr=lr,
-                    beta1=0.5,
-                    device=device)
+                     output_nc=1,
+                     lr=lr,
+                     beta1=0.5,
+                     device=device)
 ########################################
     print("Starting training loop...")
 
     for epoch in range(args.epochs):
         with tqdm(source_train_loader,desc=f'Epoch {epoch + 1 }/{args.epochs}', unit='img') as pbar:
+            target_iter = iter(target_train_loader)
+
             for data_source in pbar:   
                 try:
                     data_target = next(target_iter)
