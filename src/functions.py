8b985293be575cf96f42f63aeb2960246dfc3372
Huttunen Antti <huttuna6@haifu.org.aalto.fi>
2022-05-06 15:13:31 +0300

    fixes

--- src/functions.py
+++ src/functions.py
@@ -37,5 +37,5 @@ def dice_loss(input: Tensor, target: Tensor, multiclass: bool = False):
     # Dice loss (objective to minimize) between 0 and 1
     assert input.size() == target.size()
     fn = multiclass_dice_coeff if multiclass else dice_coeff
-    return 1 - fn(input, target, reduce_batch_first=True)
+    return 1 - fn(input.float(), target.float(), reduce_batch_first=True)
 
