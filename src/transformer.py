08a1f9d04c27525580eb6863c81ab518ddee84b4
Lauri Pohjola <lauri.pohjola@helsinki.fi>
2022-05-16 14:06:18 +0300

    Vectorized magnetball adding and add_noise_to_images

--- src/transformer.py
+++ src/transformer.py
@@ -5,6 +5,16 @@ from torchvision import transforms
 # python -m pip install -U scikit-image
 from skimage.util import random_noise
 import random
+import torchvision.transforms as T
+import torch.nn as nn
+
+def add_background_noise(image):
+
+    transforms = nn.Sequential(
+        T.ColorJitter(brightness=[0,2], hue=[-0.5,0.5], contrast=[0,3], saturation=[0,3]),
+        T.GaussianBlur(kernel_size=(51, 91), sigma=(0.1,6)))
+    return transforms(image)
+
 # function adds noise to tensors (and flips them)
 def add_noise_to_images(image, amount = 0.05):
     noise_image = image + torch.randn(image.size()) * 0.07 + 0.1 
@@ -24,6 +34,7 @@ def add_fake_magnetballs(image, mask, min_amount = 30, max_amount = 70, max_ligh
     channels, row, col = image.shape
     number_of_pixels = random.randint(min_amount, max_amount)
    
+    x_mesh,y_mesh = np.mgrid[:row,:col]
     for i in range(number_of_pixels): 
         r = random.choice([5,7,10,13])
         # Pick a random y coordinate
@@ -32,13 +43,10 @@ def add_fake_magnetballs(image, mask, min_amount = 30, max_amount = 70, max_ligh
         x_coord=random.randint(r, col - 1 - r)
         # Pick color randomly, not always as black
         black_col = np.random.uniform(0,max_lightness,1)[0]
-        for s in range(r):
-            for i in range(0,360,1):
-                x1 = s * np.cos(i)
-                y1 = s * np.sin(i)
-                #print('Before',image_temp[0, y_coord+y1.astype(int),x_coord+x1.astype(int)])
-                image_temp[0, y_coord+y1.astype(int),x_coord+x1.astype(int)] = black_col
-                mask_temp[y_coord+y1.astype(int),x_coord+x1.astype(int)] = black_col
+        distance = np.linalg.norm(np.stack([x_mesh-x_coord,y_mesh-y_coord]),axis=0)
+        mask = distance<r
+        image_temp[0, mask] = black_col
+        mask_temp[mask] = black_col
     return transforms.ToTensor()(image_temp).permute(1,2,0), transforms.ToTensor()(mask_temp).squeeze(0)
 
 def resize_image_(image, size):
